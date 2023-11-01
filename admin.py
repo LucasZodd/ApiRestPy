7b54e38ba9fb5cd96d9cf3c6fbc43304480ce1d0
Lucas Silva <91555681+LucasZodd@users.noreply.github.com>
2023-11-01 00:26:34 -0300

    Delete admin.py

--- admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import Despesa, Categoria
-
-# Registrando o modelo despesa e categoria para facilitar inserir dados
-admin.site.register(Despesa)
-admin.site.register(Categoria)
